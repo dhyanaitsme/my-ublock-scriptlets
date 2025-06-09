31942e29d3b825158a8383170f4314f5f8340921
dhyanaitsme <122237810+dhyanaitsme@users.noreply.github.com>
2025-06-09 19:05:55 +0000

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -1,6 +1,17 @@
-/// scriptlet name: github-force-dark-mode
-/// alias github-force-dark-mode aop-insert-head("style", ":root { color-scheme: dark !important; }")
+/// github-dark-mode-enabler.js
+/// alias gdme.js
 
-/*
- * Injects a dark color-scheme into GitHub for non-logged-in users.
- */
+(function() {
+  window.addEventListener("DOMContentLoaded", function() {
+    try {
+      const theme = localStorage.getItem("colorMode");
+      if (theme !== "dark") {
+        localStorage.setItem("colorMode", "dark");
+        localStorage.setItem("colorModeSync", "true");
+        location.reload();
+      }
+    } catch (e) {
+      console.error("GitHub Dark Mode Enabler failed:", e);
+    }
+  });
+})();
