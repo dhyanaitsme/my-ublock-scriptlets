9b07f5bcf5c1e9c982429863363de07a4443e10f
dhyanaitsme <122237810+dhyanaitsme@users.noreply.github.com>
2025-06-10 14:00:52 +0000

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -136,4 +136,24 @@
     });
 })();
 
+// PopularToUSBest
+function redditPopularForceUS() {
+    try {
+        const url = new URL(window.location.href);
+        // Only redirect if on main Reddit homepage or /r/popular
+        if (
+            url.hostname.endsWith('reddit.com') &&
+            (
+                url.pathname === '/' ||
+                url.pathname === '/r/popular' ||
+                url.pathname === '/r/popular/'
+            )
+        ) {
+            url.pathname = '/r/popular/best';
+            url.searchParams.set('geo_filter', 'us');
+            window.location.replace(url.toString());
+        }
+    } catch (e) {}
+}
+
 
